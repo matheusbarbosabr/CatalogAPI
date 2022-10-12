bd23c12f318642e0c1ed14ca296f2078e8b7a58b
Matheus Barbosa <matheusbarbosa.git@gmail.com>
2022-10-12 17:22:05 -0300

    First commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CatalogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().Property(p => p.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CatalogAPI.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+        public ICollection<Product>? Products { get; set; }
+    }
+}
